2caf4f938adffba32176c4c934203f91125c81ab
Saanvi <snvadhikari@gmail.com>
2024-07-08 10:30:48 +0545

    initial

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int num1 = 3;
+        int num2 = 4;
+
+        int sum = num1 + num2 ;
+
+        System.out.println("The sum is "+sum);
+
+
+
+
+    }
+}
